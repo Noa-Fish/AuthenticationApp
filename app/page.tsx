242d8aea4a4e22340222888967149504c480c4bb
Noa-Fish <95444542+Noa-Fish@users.noreply.github.com>
2024-01-16 08:38:31 +0100

    feat(login/register): login/register form

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,62 @@ import Link from "next/link"
 import axios from "axios"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { UserAuthForm } from "@/components/user-auth-form"
-
+import { set, z, ZodType } from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormLabel, FormControl, FormMessage, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { PasswordInput } from "@/components/ui/input-password";
+import { Icons } from "@/components/icons"
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 
+type FormData = {
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+const passwordRegex = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[^A-Za-z0-9]).{8,32}$/;
+const schema: ZodType<FormData> = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8)
+    .max(32)
+    .regex(passwordRegex),
+
+
+  passwordConfirmation: z
+    .string()
+    .min(8)
+    .max(32)
+    .regex(passwordRegex),
+
+})
 
 
 export default function AuthenticationPage() {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [variant, setVariant] = useState('login');
+
+  if (variant === 'register') {
+    schema.refine((data) =>
+      data.password === data.passwordConfirmation, {
+      message: "Passwords don't match",
+      path: ["passwordConfirmation"],
+    });
+  }
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+  }, []);
+
 
   // const register = useCallback(async () => {
   //   try {
@@ -28,24 +71,45 @@ export default function AuthenticationPage() {
 
   //   }
   // }, [])
-  const handleFormSubmit = (formData: any) => {
-    console.log('Form data submitted:', formData);
-    // Perform any other necessary actions with the form data
+
+
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+  })
+
+  const onSubmit = (data: FormData) => {
+    setIsLoading(true)
+    console.log(data)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
   };
 
   return (
     <>
       <div className="grid md:grid-cols-2 h-screen">
         <div className="hidden md:flex flex-col bg-zinc-900 p-10 text-white">
-          <Link
-            href="/examples/authentication"
-            className={cn(
-              buttonVariants({ variant: "ghost" }),
-              "absolute right-4 top-4 md:right-8 md:top-8 text-black"
-            )}
+          {/* <Button
+            onClick={toggleVariant}
+            variant="outline"
+            className="absolute right-4 top-4 md:right-8 md:top-8 text-black"
           >
-            Login
-          </Link>
+            {variant === 'login' ? 'Sign in' : 'Register'}
+          </Button> */}
+          <Button variant="outline" disabled={isLoading} className="absolute right-4 top-4 md:right-8 md:top-8 text-black" onClick={toggleVariant}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            {variant === 'login' ? 'Sign In' : 'Sign Up'}
+          </Button>
           <div className="text-lg font-medium flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -79,7 +143,7 @@ export default function AuthenticationPage() {
                 Create an account
               </h1>
               <p className="text-sm text-muted-foreground">
-                Sign up with your email and a&nbsp;
+                Sign up with your email and a {' '}
                 <TooltipProvider>
                   <Tooltip>
                     <TooltipTrigger><span className="font-bold">strong password</span></TooltipTrigger>
@@ -88,7 +152,7 @@ export default function AuthenticationPage() {
                         <li>1 number (0-9)</li>
                         <li>1 uppercase letters</li>
                         <li>1 lowercase letters</li>
-                        <li>1 non-alpha numeric number</li>
+                        <li>1 special character</li>
                         <li>8-32 characters with no space</li>
                       </ul>
                     </TooltipContent>
@@ -97,7 +161,119 @@ export default function AuthenticationPage() {
                 .
               </p>
             </div>
-            <UserAuthForm onFormSubmit={handleFormSubmit} />
+            <div className="grid gap-6">
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  <div className="grid gap-2">
+                    <div className="grid gap-1">
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel className="sr-only">
+                                Email
+                              </FormLabel>
+                              <FormControl>
+                                <Input
+                                  id="email"
+                                  {...field}
+                                  placeholder="Email"
+                                  type="email"
+                                />
+                              </FormControl>
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    </div>
+                    <div className="grid gap-1">
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel className="sr-only">
+                                Password
+                              </FormLabel>
+                              <FormControl>
+                                <PasswordInput
+                                  id="password"
+                                  placeholder="Password"
+                                  type="password"
+                                  {...field}
+                                />
+                              </FormControl>
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    </div>
+                    <div className="grid gap-1">
+                      <FormField
+                        control={form.control}
+                        name="passwordConfirmation"
+                        render={({ field }) => {
+                          return (
+                            <FormItem>
+                              <FormLabel className="sr-only">
+                                Password Confirmation
+                              </FormLabel>
+                              <FormControl>
+                                <PasswordInput
+                                  id="passwordConfirmation"
+                                  placeholder="Password Confirmation"
+                                  type="password"
+                                  {...field}
+                                  disabled={isLoading}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    </div>
+                    <Button type="submit" variant="outline" disabled={isLoading}>
+                      {isLoading && (
+                        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                      )}
+                      {variant === 'login' ? 'Login' : 'Register'}
+                    </Button>
+                  </div>
+                </form>
+              </Form>
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-white px-2 text-muted-foreground">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Button variant="outline" type="button" disabled={isLoading}>
+                  {isLoading ? (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  ) : (
+                    <Icons.google className="mr-2 h-4 w-4" />
+                  )}{" "}
+                  Google
+                </Button>
+                <Button variant="outline" type="button" disabled={isLoading}>
+                  {isLoading ? (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  ) : (
+                    <Icons.gitHub className="mr-2 h-4 w-4" />
+                  )}{" "}
+                  GitHub
+                </Button>
+              </div>
+            </div >
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
               <Link
